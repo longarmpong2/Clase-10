97c2bc9e4f6f20afc51a6686ecf932c9be71a12a
longarmpong2 <97767883+longarmpong2@users.noreply.github.com>
2022-01-14 18:04:48 -0600

    Add files via upload

---
+++ SATemplate-main/miprimerprograma/sketch.js
@@ -0,0 +1,27 @@
+console.log("hola");
+console.log("como estas");
+console.log("bien y tu");
+console.log("bien tambien");
+console.log("que hiciste hoy");
+console.log("muchas cosas");
+console.log("como que");
+console.log("pues muchas");
+console.log("eeeh");
+console.log("yo tambien hice varias cosas");
+console.log("enserio");
+console.log("no");
+console.log("ah");
+console.log("hehe");
+console.log("hehe");
+
+
+
+
+
+
+
+
+
+
+
+
